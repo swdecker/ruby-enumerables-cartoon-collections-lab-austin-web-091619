0e3ddefba34169a5a4fd01870eca03cddc85c119
Steven Decker <swdecker@gmail.com>
2019-09-01 01:44:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
-dwarves = [tiny, biny, sminy]
+dwarves = ["tiny", "biny", "sminy"]
 roll_call_dwarves(dwarves)
