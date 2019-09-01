878c3a86aca5f1c5a56b2ce93908c11fa848d186
Steven Decker <swdecker@gmail.com>
2019-09-01 02:21:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,8 +23,8 @@ end
 def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  arr.index{
-  arr.find {|cheese| arr.include?(cheese)}
+  
+  arr.find {|item| cheese_types.include?(item)}
 end
 
 #dwarves = ["tiny", "biny", "sminy"]
