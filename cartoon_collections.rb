1df730828ed9e40d04196247863d0305e658386a
Steven Decker <swdecker@gmail.com>
2019-09-01 02:11:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,10 @@ def long_planeteer_calls(call_array)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
 
 #dwarves = ["tiny", "biny", "sminy"]
